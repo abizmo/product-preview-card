f018c753adadf6277490333fd0dbdba5b42942e6
abizmo <abizmo@gmail.com>
2023-02-24 12:00:39 +0000

    fix: rename secondary color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     colors: {
       primary: 'hsl(158, 36%, 37%)',
-      cream: 'hsl(30, 38%, 92%)',
+      secondary: 'hsl(30, 38%, 92%)',
       'neutral-900': 'hsl(212, 21%, 14%)',
       'neutral-600': 'hsl(228, 12%, 48%)',
       'neutral-100': 'hsl(0, 0%, 100%)',
