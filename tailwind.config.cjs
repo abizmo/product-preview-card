69e2c9a53b3716f10a50f7fb48058217f9b28339
abizmo <abizmo@gmail.com>
2023-02-24 11:06:34 +0000

    add theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    colors: {
+      primary: 'hsl(158, 36%, 37%)',
+      cream: 'hsl(30, 38%, 92%)',
+      'neutral-900': 'hsl(212, 21%, 14%)',
+      'neutral-600': 'hsl(228, 12%, 48%)',
+      'neutral-100': 'hsl(0, 0%, 100%)',
+    },
     extend: {},
   },
   plugins: [],
